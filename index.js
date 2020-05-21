73d7d227908c73fa2945aece606fa8fd8367ebc0
sang hyun Jeon <jsh0813@gmail.com>
2020-05-21 15:34:03 +0900

    morgan, helmet, cookie-parser, body-parser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// const express = require('express');
 import express from "express";
+import morgannamesomething from "morgan";
+import helmetnamesomething from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -10,12 +14,15 @@ const handleHome = (req, res) =>res.send("hello from home respond");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome =(req, res, next) => {
-    console.log('between');
-    next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmetnamesomething());
+app.use(morgannamesomething("dev"));
 
-app.use(betweenHome);
+const middleware =(req, res, next) =>{
+    res.send("not happening");
+};
 
 app.get("/", handleHome);
 
