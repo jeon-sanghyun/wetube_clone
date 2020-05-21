8ee0fed855a5418c4cac1baa39f3bc70cd336d8f
sang hyun Jeon <jsh0813@gmail.com>
2020-05-21 14:29:28 +0900

    babel, nodemon,

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express');
+// const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening () {
-    console.log(`Listening on: http://localhost:${PORT}`)
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res){
-    console.log("hi from home!!@!@!@!@");
-    res.send("hello from home respond");
-}
+// function handleListening () {
+//     console.log(`Listening on: http://localhost:${PORT}`)
+// }
 
-function handleProfile(req, res){
-    res.send("You are on my profile");
-}
+const handleHome = (req, res) =>res.send("hello from home respond");
+
+// function handleHome(req, res){
+//     console.log("hi from home!!@!@!@!@");
+//     res.send("hello from home respond");
+// }
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+// function handleProfile(req, res){
+//     res.send("You are on my profile");
+// }
 
 app.get("/", handleHome);
 
