b3c28426b1e1e556b5be472a31db521ce66acbff
sang hyun Jeon <jsh0813@gmail.com>
2020-05-21 22:12:24 +0900

    globalRoute, userRoute, videoRoute

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgannamesomething from "morgan";
 import helmetnamesomething from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) =>res.send("hello from home respond");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmetnamesomething());
 app.use(morgannamesomething("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use('/user', userRouter);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
