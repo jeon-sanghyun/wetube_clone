3f549f59b3c694430aaabef18129d9827b49aa07
sang hyun Jeon <jsh0813@gmail.com>
2020-05-22 15:25:56 +0900

    middleware locals

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import morgannamesomething from "morgan";
 import helmetnamesomething from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmetnamesomething());
 app.use(morgannamesomething("dev"));
+app.use(localMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
