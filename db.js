9800e3033ad530a52a1ddb79a75b7084c14d9d0f
sang hyun Jeon <jsh0813@gmail.com>
2020-05-25 15:19:31 +0900

    db, each x in y,

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+    {
+        id:324393,
+        title:'Video awesome',
+        description:'This is something I love',
+        views:24,
+        videoFile:'https://archive.org/details/BigBuckBunny_124',
+        creator:{
+            id:12121,
+            name:"abc name",
+            email:"fake@asdf.com",
+        }
+    },
+    {
+        id:456,
+        title:'Video super',
+        description:'This is something I love',
+        views:24,
+        videoFile:'https://archive.org/details/BigBuckBunny_124',
+        creator:{
+            id:12121,
+            name:"abc name",
+            email:"fake@asdf.com",
+        }
+    },
+    {
+        id:5678,
+        title:'Video clone',
+        description:'This is something I love',
+        views:24,
+        videoFile:'https://archive.org/details/BigBuckBunny_124',
+        creator:{
+            id:12121,
+            name:"abc name",
+            email:"fake@asdf.com",
+        }
+    },
+    {
+        id:143,
+        title:'Video lalala',
+        description:'This is something I love',
+        views:24,
+        videoFile:'https://archive.org/details/BigBuckBunny_124',
+        creator:{
+            id:12121,
+            name:"abc name",
+            email:"fake@asdf.com",
+        }
+    },
+    {
+        id:2462,
+        title:'Video asdf',
+        description:'This is something I love',
+        views:24,
+        videoFile:'https://archive.org/details/BigBuckBunny_124',
+        creator:{
+            id:12121,
+            name:"abc name",
+            email:"fake@asdf.com",
+        }
+    },
+]
