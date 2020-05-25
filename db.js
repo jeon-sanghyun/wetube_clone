a56dd40afe0fb354da2797fbdb1853d59bea161f
sang hyun Jeon <jsh0813@gmail.com>
2020-05-25 23:42:51 +0900

    mongodb

--- db.js
+++ db.js
@@ -1,62 +1,17 @@
-export const videos = [
+import mongoose from "mongoose";
+
+
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
     {
-        id:324393,
-        title:'Video awesome',
-        description:'This is something I love',
-        views:24,
-        videoFile:"hhttps://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:12121,
-            name:"abc name",
-            email:"fake@asdf.com",
-        }
-    },
-    {
-        id:456,
-        title:'Video super',
-        description:'This is something I love',
-        views:24,
-        videoFile:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:12121,
-            name:"abc name",
-            email:"fake@asdf.com",
-        }
-    },
-    {
-        id:5678,
-        title:'Video clone',
-        description:'This is something I love',
-        views:24,
-        videoFile:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:12121,
-            name:"abc name",
-            email:"fake@asdf.com",
-        }
-    },
-    {
-        id:143,
-        title:'Video lalala',
-        description:'This is something I love',
-        views:24,
-        videoFile:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:12121,
-            name:"abc name",
-            email:"fake@asdf.com",
-        }
-    },
-    {
-        id:2462,
-        title:'Video asdf',
-        description:'This is something I love',
-        views:24,
-        videoFile:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:12121,
-            name:"abc name",
-            email:"fake@asdf.com",
-        }
-    },
-]
+        useNewUrlParser: true, 
+        useFindAndModify:false
+    });
+
+    const db = mongoose.connection;
+
+    const handlerOpen = () => console.log("✅ Connect to DB");
+    const handlerError = (error) => console.log(`❌ Error on DB Connection:${error}`);
+
+    db.once("open", handlerOpen);
+    db.on("error", handlerError);
