4ea4267ec9c4558baf16c2e2031187ba39a3ed20
sang hyun Jeon <jsh0813@gmail.com>
2020-05-27 13:28:58 +0900

    eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    // ecmaVersion: 11,
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+    "spaced-comment": "off",
+    "no-else-return": "off",
+  },
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,41 @@
 import routes from "../routes";
 
-export const getJoin = (req,res) => {
-    res.render("join", {pageTitle: "Join"});
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req,res) => {
-    const{
-        body:{name, email, password, password2}
-    } = req;
-    if(password !== password2){
-        res.status(400);
-        res,render("join",{ pageTitle:"Join"});
-    } else{
-        //to do: registor User
-        //to do: Log user in
-        res.redirect(routes.home)
-    }
-    res.render("join", {pageTitle: "Join"});
-}
-
-export const getLogin = (req,res) => {
-    res.render("login", {pageTitle: "Login"});
-};
-
-export const postLogin =(req,res) =>{
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    //to do: registor User
+    //to do: Log user in
     res.redirect(routes.home);
+  }
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const logout = (req,res) => {
-//to do: process logout
-    res.redirect(routes.home);
+export const getLogin = (req, res) => {
+  res.render("login", { pageTitle: "Login" });
 };
 
-export const users = (req,res) => res.render("users", {pageTitle: "Users"});
-export const userDetail = (req,res) => res.render("userDetail", {pageTitle: "User Detail"});
-export const editProfile = (req,res) => res.render("editProfile", {pageTitle: "Edit Profile"});
-export const changePassword = (req,res) => res.render("changePassword", {pageTitle: "Change Password"});
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  //to do: process logout
+  res.redirect(routes.home);
+};
+
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
