007d82b71ffee18a0eb13e37e0568f2ed0437b3a
sang hyun Jeon <jsh0813@gmail.com>
2020-06-01 17:10:50 +0900

    postGithublogin, routes.

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
@@ -7,8 +8,10 @@ import {
   getLogin,
   postJoin,
   postLogin,
+  githubLogin,
+  postGithubLogin,
 } from "../controllers/userController";
-import { onlyPublic } from "../middlewares";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
@@ -21,6 +24,14 @@ globalRouter.post(routes.login, postLogin);
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogin
+);
 
 export default globalRouter;
